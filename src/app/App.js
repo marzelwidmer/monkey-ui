62e875b083a37acae61c122e7068a739175f9fa9
Marcel Widmer <marzelwidmer@gmail.com>
2019-02-16 06:45:05 +0100

    update readme

--- src/app/App.js
+++ src/app/App.js
@@ -1,26 +1,15 @@
 import React, { Component } from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
-import TopNav from "../layout/TopNav"
 import "./App.css"
-import BlueGreen from "../bluegreen/BlueGreen"
-import AbDeployment from '../ab-deployment/AbDeployment';
-import AbTesting from '../ab-testing/AbTesting';
 
  
 class App extends Component {
   render() {
-    document.title = "MonkeyApp"
+    document.title = "MonkeyUI"
     return (
       <div>
-        <TopNav />
 
-        <div>
-          
-          <BlueGreen />
-          <AbDeployment />
-          <AbTesting />
-
-        </div>
+    
       </div>
     )
   }
